c6b01ce69364b5f8623a99dd7f7aab1949951d00
imndx <imndxx@gmail.com>
2019-01-22 21:09:03 +0800

    update conversation portrait

--- chat/src/main/java/cn/wildfire/chat/conversationlist/viewholder/ChannelConversationViewHolder.java
+++ chat/src/main/java/cn/wildfire/chat/conversationlist/viewholder/ChannelConversationViewHolder.java
@@ -1,12 +1,20 @@
 package cn.wildfire.chat.conversationlist.viewholder;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import cn.wildfire.chat.GlideApp;
 import cn.wildfire.chat.annotation.ConversationInfoType;
 import cn.wildfire.chat.annotation.EnableContextMenu;
+import cn.wildfirechat.chat.R;
+import cn.wildfirechat.model.ChannelInfo;
 import cn.wildfirechat.model.Conversation;
+import cn.wildfirechat.model.ConversationInfo;
+import cn.wildfirechat.remote.ChatManager;
 
 @ConversationInfoType(type = Conversation.ConversationType.Channel, line = 0)
 @EnableContextMenu
@@ -14,6 +22,20 @@ public class ChannelConversationViewHolder extends ConversationViewHolder {
 
     public ChannelConversationViewHolder(Fragment fragment, RecyclerView.Adapter adapter, View itemView) {
         super(fragment, adapter, itemView);
+    }
+
+    @Override
+    protected void onBindConversationInfo(ConversationInfo conversationInfo) {
+        ChannelInfo channelInfo = ChatManager.Instance().getChannelInfo(conversationInfo.conversation.target, false);
+        if (channelInfo != null) {
+            nameTextView.setText(channelInfo.name);
+            GlideApp
+                    .with(fragment)
+                    .load(channelInfo.portrait)
+                    .placeholder(R.mipmap.ic_group_cheat)
+                    .transforms(new CenterCrop(), new RoundedCorners(10))
+                    .into(portraitImageView);
+        }
     }
 
 }
--- chat/src/main/java/cn/wildfire/chat/conversationlist/viewholder/GroupConversationViewHolder.java
+++ chat/src/main/java/cn/wildfire/chat/conversationlist/viewholder/GroupConversationViewHolder.java
@@ -1,12 +1,20 @@
 package cn.wildfire.chat.conversationlist.viewholder;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import cn.wildfire.chat.ChatManagerHolder;
+import cn.wildfire.chat.GlideApp;
 import cn.wildfire.chat.annotation.ConversationInfoType;
 import cn.wildfire.chat.annotation.EnableContextMenu;
+import cn.wildfirechat.chat.R;
 import cn.wildfirechat.model.Conversation;
+import cn.wildfirechat.model.ConversationInfo;
+import cn.wildfirechat.model.GroupInfo;
 
 @ConversationInfoType(type = Conversation.ConversationType.Group, line = 0)
 @EnableContextMenu
@@ -14,6 +22,21 @@ public class GroupConversationViewHolder extends ConversationViewHolder {
 
     public GroupConversationViewHolder(Fragment fragment, RecyclerView.Adapter adapter, View itemView) {
         super(fragment, adapter, itemView);
+    }
+
+    @Override
+    protected void onBindConversationInfo(ConversationInfo conversationInfo) {
+        GroupInfo groupInfo = ChatManagerHolder.gChatManager.getGroupInfo(conversationInfo.conversation.target, false);
+        if (groupInfo != null) {
+            GlideApp
+                    .with(fragment)
+                    .load(groupInfo.portrait)
+                    .placeholder(R.mipmap.ic_group_cheat)
+                    .transforms(new CenterCrop(), new RoundedCorners(10))
+                    .into(portraitImageView);
+            //群昵称
+            nameTextView.setText(groupInfo.name);
+        }
     }
 
 }
