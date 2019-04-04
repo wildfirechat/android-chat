ac438fb9d86a8c0e59fa1ec4c0269c2fca02a7ea
imndx <imndxx@gmail.com>
2019-04-04 18:14:37 +0800

    crash when selecting downloaded file on certain devices. Fix #3

--- chat/src/main/java/cn/wildfire/chat/kit/utils/FilePathUtil.java
+++ chat/src/main/java/cn/wildfire/chat/kit/utils/FilePathUtil.java
@@ -1,5 +1,6 @@
 package cn.wildfire.chat.kit.utils;
 
+
 import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.Context;
@@ -9,74 +10,29 @@ import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FilePathUtil {
 
-    public static String getRealPath(Context context, Uri uri) {
-        if (uri == null) {
-            return null;
-        }
-        String path;
-        if ("file".equalsIgnoreCase(uri.getScheme())) {//使用第三方应用打开
-            path = uri.getPath();
-        } else {
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {//4.4以后
-                path = getPath(context, uri);
-            } else {//4.4以下下系统调用方法
-                path = getRealPathFromURI(context, uri);
-            }
+    public static String getPathFromUri(final Context context, final Uri uri) {
+        String path = getPathFromLocalUri(context, uri);
+        if (path == null) {
+            path = getPathFromRemoteUri(context, uri);
         }
         return path;
     }
 
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    private static String getRealPathFromURI(Context context, Uri contentUri) {
-        String res = null;
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-        if (null != cursor && cursor.moveToFirst()) {
-            ;
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            res = cursor.getString(column_index);
-            cursor.close();
-        }
-        return res;
-    }
-
-    /**
-     * 专为Android4.4设计的从Uri获取文件绝对路径，以前的方法已不好使
-     */
     @SuppressLint("NewApi")
-    private static String getPath(final Context context, final Uri uri) {
-
+    private static String getPathFromLocalUri(final Context context, final Uri uri) {
         final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
 
-        // DocumentProvider
         if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
             if (isExternalStorageDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
@@ -85,18 +41,21 @@ public class FilePathUtil {
                 if ("primary".equalsIgnoreCase(type)) {
                     return Environment.getExternalStorageDirectory() + "/" + split[1];
                 }
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
+            } else if (isDownloadsDocument(uri)) {
                 final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
 
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
+                if (!TextUtils.isEmpty(id)) {
+                    try {
+                        final Uri contentUri =
+                                ContentUris.withAppendedId(
+                                        Uri.parse(Environment.DIRECTORY_DOWNLOADS), Long.valueOf(id));
+                        return getDataColumn(context, contentUri, null, null);
+                    } catch (NumberFormatException e) {
+                        return null;
+                    }
+                }
+
+            } else if (isMediaDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
                 final String type = split[0];
@@ -115,38 +74,30 @@ public class FilePathUtil {
 
                 return getDataColumn(context, contentUri, selection, selectionArgs);
             }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri)) {
+                return uri.getLastPathSegment();
+            }
+
             return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
             return uri.getPath();
         }
+
         return null;
     }
 
-    /**
-     * Get the value of the data column for this Uri. This is useful for
-     * MediaStore Uris, and other file-based ContentProviders.
-     *
-     * @param context       The context.
-     * @param uri           The Uri to query.
-     * @param selection     (Optional) Filter used in the query.
-     * @param selectionArgs (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
-    private static String getDataColumn(Context context, Uri uri, String selection,
-                                        String[] selectionArgs) {
-
+    private static String getDataColumn(
+            Context context, Uri uri, String selection, String[] selectionArgs) {
         Cursor cursor = null;
+
         final String column = "_data";
         final String[] projection = {column};
 
         try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
             if (cursor != null && cursor.moveToFirst()) {
                 final int column_index = cursor.getColumnIndexOrThrow(column);
                 return cursor.getString(column_index);
@@ -157,5 +108,62 @@ public class FilePathUtil {
             }
         }
         return null;
+    }
+
+    private static String getPathFromRemoteUri(final Context context, final Uri uri) {
+        // The code below is why Java now has try-with-resources and the Files utility.
+        File file = null;
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        boolean success = false;
+        try {
+            inputStream = context.getContentResolver().openInputStream(uri);
+            file = File.createTempFile("file_picker", "jpg", context.getCacheDir());
+            outputStream = new FileOutputStream(file);
+            if (inputStream != null) {
+                copy(inputStream, outputStream);
+                success = true;
+            }
+        } catch (IOException ignored) {
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close();
+            } catch (IOException ignored) {
+            }
+            try {
+                if (outputStream != null) outputStream.close();
+            } catch (IOException ignored) {
+                // If closing the output stream fails, we cannot be sure that the
+                // target file was written in full. Flushing the stream merely moves
+                // the bytes into the OS, not necessarily to the file.
+                success = false;
+            }
+        }
+        return success ? file.getPath() : null;
+    }
+
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        final byte[] buffer = new byte[4 * 1024];
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
+        out.flush();
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 }
