17199e43c195477f785814b7b9b0c83a95f6c7e0
imndx <imndxx@gmail.com>
2019-05-13 19:24:45 +0800

    revert bugly key

--- chat/src/main/java/cn/wildfire/chat/app/MyApp.java
+++ chat/src/main/java/cn/wildfire/chat/app/MyApp.java
@@ -19,7 +19,7 @@ public class MyApp extends BaseApp {
         super.onCreate();
 
         // bugly，务必替换为你自己的!!!
-        CrashReport.initCrashReport(getApplicationContext(), "your bugly key", false);
+        CrashReport.initCrashReport(getApplicationContext(), "34490ba79f", false);
         // 只在主进程初始化
         if (getCurProcessName(this).equals("cn.wildfirechat.chat")) {
             wfcUIKit = new WfcUIKit();
