6df9a923168e4f185816ab54159d5bc7729631b6
imndx <imndxx@gmail.com>
2019-05-15 18:32:55 +0800

    add errorCode and fix recall issue

---
+++ client/src/main/java/cn/wildfirechat/ErrorCode.java
@@ -0,0 +1,8 @@
+package cn.wildfirechat;
+
+public class ErrorCode {
+    public static final int SERVICE_EXCEPTION = -1000;
+    public static final int SERVICE_DIED = -1001;
+    public static final int FILE_NOT_EXIST = -1002;
+    public static final int FILE_TOO_LARGE = -1003;
+}
