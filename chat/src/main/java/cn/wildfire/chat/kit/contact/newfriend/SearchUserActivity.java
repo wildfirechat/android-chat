224e99ea154d6fecf9c55c80c61e4f4dbd8afc8d
imndx <imndxx@gmail.com>
2019-06-22 00:20:07 +0800

    refactor search user.

--- chat/src/main/java/cn/wildfire/chat/kit/contact/newfriend/SearchUserActivity.java
+++ chat/src/main/java/cn/wildfire/chat/kit/contact/newfriend/SearchUserActivity.java
@@ -1,70 +1,18 @@
 package cn.wildfire.chat.kit.contact.newfriend;
 
-import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.List;
 
-import androidx.appcompat.widget.SearchView;
-import cn.wildfire.chat.kit.WfcBaseActivity;
-import cn.wildfirechat.chat.R;
+import cn.wildfire.chat.kit.search.SearchActivity;
+import cn.wildfire.chat.kit.search.SearchableModule;
 
-public class SearchUserActivity extends WfcBaseActivity {
-
-    private SearchView searchView;
-    private SearchUserFragment searchUserFragment;
+public class SearchUserActivity extends SearchActivity {
 
     @Override
-    protected int contentLayout() {
-        return R.layout.fragment_container_activity;
+    protected void beforeViews() {
     }
 
     @Override
-    protected int menu() {
-        return R.menu.search_user;
+    protected void initSearchModule(List<SearchableModule> modules) {
+        modules.add(new UserSearchModule());
     }
-
-    @Override
-    protected void afterMenus(Menu menu) {
-        super.afterMenus(menu);
-        MenuItem searchItem = menu.findItem(R.id.search);
-        //通过MenuItem得到SearchView
-        searchView = (SearchView) searchItem.getActionView();
-        initSearchView();
-    }
-
-    private void initSearchView() {
-        searchView.onActionViewExpanded();
-        searchView.setQueryHint("Search");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                search(s);
-                return true;
-            }
-        });
-
-    }
-
-    @Override
-    protected void afterViews() {
-        searchUserFragment = new SearchUserFragment();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.containerFrameLayout, searchUserFragment)
-                .commit();
-    }
-
-    private void search(String keyword) {
-        if (!TextUtils.isEmpty(keyword)) {
-            searchUserFragment.showSearchPromptView(keyword);
-        } else {
-            searchUserFragment.hideSearchPromptView();
-        }
-    }
-
 }
