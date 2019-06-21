30844708c71d62cb1aee6dbd46263f4fe0a26eb4
imndx <imndxx@gmail.com>
2019-06-21 13:20:33 +0800

    update friend request list. fix #40

--- chat/src/main/java/cn/wildfire/chat/kit/contact/newfriend/FriendRequestListAdapter.java
+++ chat/src/main/java/cn/wildfire/chat/kit/contact/newfriend/FriendRequestListAdapter.java
@@ -4,12 +4,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
 import cn.wildfirechat.chat.R;
 import cn.wildfirechat.model.FriendRequest;
+import cn.wildfirechat.model.UserInfo;
 
 public class FriendRequestListAdapter extends RecyclerView.Adapter<FriendRequestViewHolder> {
     private List<FriendRequest> friendRequests;
@@ -29,6 +31,19 @@ public class FriendRequestListAdapter extends RecyclerView.Adapter<FriendRequest
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.contact_item_new_friend, parent, false);
         return new FriendRequestViewHolder(fragment, this, view);
+    }
+
+    public void onUserInfosUpdate(List<UserInfo> userInfos) {
+        if (friendRequests == null || friendRequests.isEmpty()) {
+            return;
+        }
+        for (UserInfo info : userInfos) {
+            for (int i = 0; i < friendRequests.size(); i++) {
+                if (friendRequests.get(i).target.equals(info.uid)) {
+                    notifyItemChanged(i);
+                }
+            }
+        }
     }
 
     @Override
