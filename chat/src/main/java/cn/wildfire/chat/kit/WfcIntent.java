836b3b273f5c943808ff7b71424a200c639ce457
imndx <imndxx@gmail.com>
2019-06-25 18:06:48 +0800

    refactor.

--- chat/src/main/java/cn/wildfire/chat/kit/WfcIntent.java
+++ chat/src/main/java/cn/wildfire/chat/kit/WfcIntent.java
@@ -1,10 +1,12 @@
 package cn.wildfire.chat.kit;
 
+import cn.wildfirechat.chat.BuildConfig;
+
 public interface WfcIntent {
-    String ACTION_MAIN = "cn.wildfirechat.chat.main";
-    String ACTION_CONVERSATION = "cn.wildfirechat.chat.conversation";
-    String ACTION_CONTACT = "cn.wildfirechat.chat.contact";
-    String ACTION_USER_INFO = "cn.wildfirechat.chat.user.info";
-    String ACTION_GROUP_INFO = "cn.wildfirechat.chat.group.info";
-    String ACTION_VOIP_SINGLE = "cn.wildfirechat.kit.chat.voip.single";
+    String ACTION_MAIN = BuildConfig.APPLICATION_ID + ".main";
+    String ACTION_CONVERSATION = BuildConfig.APPLICATION_ID + ".conversation";
+    String ACTION_CONTACT = BuildConfig.APPLICATION_ID + ".contact";
+    String ACTION_USER_INFO = BuildConfig.APPLICATION_ID + ".user.info";
+    String ACTION_GROUP_INFO = BuildConfig.APPLICATION_ID + ".group.info";
+    String ACTION_VOIP_SINGLE = BuildConfig.APPLICATION_ID + ".kit.voip.single";
 }
