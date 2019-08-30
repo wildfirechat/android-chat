5386148f3ceb5b76f80d175b43bbc29535a765fe
imndx <imndxx@gmail.com>
2019-08-31 00:12:39 +0800

    load url in WfcWebViewActivity

--- chat/src/main/java/cn/wildfire/chat/kit/WfcIntent.java
+++ chat/src/main/java/cn/wildfire/chat/kit/WfcIntent.java
@@ -9,4 +9,5 @@ public interface WfcIntent {
     String ACTION_USER_INFO = BuildConfig.APPLICATION_ID + ".user.info";
     String ACTION_GROUP_INFO = BuildConfig.APPLICATION_ID + ".group.info";
     String ACTION_VOIP_SINGLE = BuildConfig.APPLICATION_ID + ".kit.voip.single";
+    String ACTION_VIEW = BuildConfig.APPLICATION_ID + ".webview";
 }
