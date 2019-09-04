0be73b91d6c9c6d2f221ed3759b69491de8a11c0
imndx <imndxx@gmail.com>
2019-09-04 20:06:30 +0800

    show default portrait when load portrait error

--- chat/src/main/java/cn/wildfire/chat/kit/search/viewHolder/MessageViewHolder.java
+++ chat/src/main/java/cn/wildfire/chat/kit/search/viewHolder/MessageViewHolder.java
@@ -6,22 +6,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.wildfire.chat.kit.GlideApp;
 import cn.wildfire.chat.kit.WfcUIKit;
-import cn.wildfire.chat.kit.group.GroupViewModel;
 import cn.wildfire.chat.kit.third.utils.TimeUtils;
 import cn.wildfire.chat.kit.user.UserViewModel;
 import cn.wildfirechat.chat.R;
 import cn.wildfirechat.message.Message;
 import cn.wildfirechat.message.notification.NotificationMessageContent;
-import cn.wildfirechat.model.Conversation;
 import cn.wildfirechat.model.UserInfo;
 
 public class MessageViewHolder extends RecyclerView.ViewHolder {
@@ -44,7 +39,6 @@ public class MessageViewHolder extends RecyclerView.ViewHolder {
         ButterKnife.bind(this, itemView);
     }
 
-    // fixme display name
     public void onBind(Message message) {
         UserInfo sender = userViewModel.getUserInfo(message.sender, false);
         if (sender != null) {
@@ -53,12 +47,7 @@ public class MessageViewHolder extends RecyclerView.ViewHolder {
             } else {
                 nameTextView.setText("<" + sender.uid + ">");
             }
-            String name;
-            if (message.conversation.type == Conversation.ConversationType.Group) {
-                GroupViewModel groupViewModel = ViewModelProviders.of(fragment).get(GroupViewModel.class);
-                name = groupViewModel.getGroupMemberDisplayName(message.conversation.target, message.sender);
-            }
-            Glide.with(fragment).load(sender.portrait).apply(new RequestOptions().placeholder(R.mipmap.default_header)).into(portraitImageView);
+            GlideApp.with(portraitImageView).load(sender.portrait).error(R.mipmap.default_header).into(portraitImageView);
         }
         if (message.content instanceof NotificationMessageContent) {
             contentTextView.setText(((NotificationMessageContent) message.content).formatNotification(message));
