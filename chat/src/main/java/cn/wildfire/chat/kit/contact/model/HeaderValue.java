d592adcc3ac3f462add2d40726cdadbda0402c53
imndx <imndxx@gmail.com>
2019-09-07 00:43:10 +0800

    update contact

--- chat/src/main/java/cn/wildfire/chat/kit/contact/model/HeaderValue.java
+++ chat/src/main/java/cn/wildfire/chat/kit/contact/model/HeaderValue.java
@@ -1,4 +1,10 @@
 package cn.wildfire.chat.kit.contact.model;
 
+import androidx.annotation.Nullable;
+
 public class HeaderValue {
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return obj instanceof HeaderValue;
+    }
 }
