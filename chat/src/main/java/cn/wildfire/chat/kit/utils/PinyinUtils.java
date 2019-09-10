f58ceb644b028fec0050465b6eeb321be859e823
imndx <imndxx@gmail.com>
2019-09-10 15:35:37 +0800

    fix crash when user name contains special character

--- chat/src/main/java/cn/wildfire/chat/kit/utils/PinyinUtils.java
+++ chat/src/main/java/cn/wildfire/chat/kit/utils/PinyinUtils.java
@@ -5,7 +5,6 @@ import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 public class PinyinUtils {
 
@@ -37,12 +36,12 @@ public class PinyinUtils {
                 // 肯定不是汉字
                 sb.append(c);
             } else {
-                String s = "";
+                String s = "#";
                 try {
                     // 通过char得到拼音集合. 单 -> dan, shan 
                     s = PinyinHelper.toHanyuPinyinStringArray(c, format)[0];
                     sb.append(s);
-                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                     sb.append(s);
                 }
