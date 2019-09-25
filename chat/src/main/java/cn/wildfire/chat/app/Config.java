63fd4fa032e55604411859c74d3c1161fdf90b26
imndx <imndxx@gmail.com>
2019-09-25 15:24:14 +0800

    config validate

--- chat/src/main/java/cn/wildfire/chat/app/Config.java
+++ chat/src/main/java/cn/wildfire/chat/app/Config.java
@@ -1,6 +1,8 @@
 package cn.wildfire.chat.app;
 
 import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
 
 /**
  * Created by heavyrain lee on 2017/11/24.
@@ -8,6 +10,8 @@ import android.os.Environment;
 
 public interface Config {
 
+    // 仅仅是host，没有http开头
+    // 不可配置为127.0.0.1 或者 192.168.0.1
     String IM_SERVER_HOST = "wildfirechat.cn";
     int IM_SERVER_PORT = 80;
 
@@ -24,4 +28,22 @@ public interface Config {
     String AUDIO_SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/wfc/audio";
     String PHOTO_SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/wfc/photo";
     String FILE_SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/wfc/file";
+
+    static void validateConfig() {
+        if (TextUtils.isEmpty(IM_SERVER_HOST)
+                || IM_SERVER_HOST.startsWith("http")
+                || TextUtils.isEmpty(APP_SERVER_ADDRESS)
+                || !APP_SERVER_ADDRESS.startsWith("http")
+                || IM_SERVER_HOST.equals("192.168.0.1")
+                || IM_SERVER_HOST.equals("127.0.0.1")
+                || APP_SERVER_ADDRESS.contains("192.168.0.1")
+                || APP_SERVER_ADDRESS.contains("127.0.0.1")
+        ) {
+            throw new IllegalStateException("im server host config error");
+        }
+
+        if (IM_SERVER_PORT != 80) {
+            Log.w("wfc config", "如果IM_SERVER_PORT配置为非80端口，无法使用第三方文件存储");
+        }
+    }
 }
