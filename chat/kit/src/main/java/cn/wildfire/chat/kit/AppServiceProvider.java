b2899daf4139f0a708f2186a1cece69e8b4ba5de
imndx <imndxx@gmail.com>
2019-10-24 16:29:20 +0800

    add group announcement

--- chat/kit/src/main/java/cn/wildfire/chat/kit/AppServiceProvider.java
+++ chat/kit/src/main/java/cn/wildfire/chat/kit/AppServiceProvider.java
@@ -1,24 +1,19 @@
 package cn.wildfire.chat.kit;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import cn.wildfire.chat.app.AppService;
-import cn.wildfire.chat.app.Config;
 import cn.wildfire.chat.kit.group.GroupAnnouncement;
-import cn.wildfire.chat.kit.net.OKHttpHelper;
-import cn.wildfire.chat.kit.net.SimpleCallback;
-
 
 
 public interface AppServiceProvider {
     public interface UpdateGroupAnnouncementCallback {
         void onUiSuccess(GroupAnnouncement announcement);
+
         void onUiFailure(int code, String msg);
     }
 
     public interface GetGroupAnnouncementCallback {
         void onUiSuccess(GroupAnnouncement announcement);
+
         void onUiFailure(int code, String msg);
     }
 
