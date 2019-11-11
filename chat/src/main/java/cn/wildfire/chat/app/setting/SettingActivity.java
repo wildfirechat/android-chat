1640ab7a2f0d576fd9d8ec5677d1ca8b99235193
imndx <imndxx@gmail.com>
2019-11-11 18:40:24 +0800

    add server diagnose

--- chat/src/main/java/cn/wildfire/chat/app/setting/SettingActivity.java
+++ chat/src/main/java/cn/wildfire/chat/app/setting/SettingActivity.java
@@ -3,14 +3,22 @@ package cn.wildfire.chat.app.setting;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.OnClick;
+import cn.wildfire.chat.app.Config;
+import cn.wildfire.chat.app.main.SplashActivity;
 import cn.wildfire.chat.kit.ChatManagerHolder;
 import cn.wildfire.chat.kit.WfcBaseActivity;
-import cn.wildfire.chat.app.main.SplashActivity;
+import cn.wildfire.chat.kit.net.OKHttpHelper;
+import cn.wildfire.chat.kit.net.SimpleCallback;
+import cn.wildfire.chat.kit.widget.OptionItemView;
 import cn.wildfirechat.chat.R;
 
 public class SettingActivity extends WfcBaseActivity {
+    @BindView(R.id.diagnoseOptionItemView)
+    OptionItemView diagnoseOptionItemView;
 
     @Override
     protected int contentLayout() {
@@ -31,6 +39,26 @@ public class SettingActivity extends WfcBaseActivity {
 
     @OnClick(R.id.newMsgNotifyOptionItemView)
     void notifySetting() {
+
+    }
+
+    @OnClick(R.id.diagnoseOptionItemView)
+    void diagnose() {
+        long start = System.currentTimeMillis();
+        OKHttpHelper.get("http://" + Config.IM_SERVER_HOST + ":" + Config.IM_SERVER_PORT + "/api/version", null, new SimpleCallback<String>() {
+            @Override
+            public void onUiSuccess(String s) {
+                long duration = (System.currentTimeMillis() - start) / 2;
+                diagnoseOptionItemView.setDesc(duration + "ms");
+                Toast.makeText(SettingActivity.this, "服务器延迟为：" + duration + "ms", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUiFailure(int code, String msg) {
+                diagnoseOptionItemView.setDesc("test failed");
+                Toast.makeText(SettingActivity.this, "访问IM Server失败", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
