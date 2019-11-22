740a7f96b63c77d1b434405c895e25e284ba6475
heavyrain2012 <heavyrain.lee@gmail.com>
2019-11-22 11:46:00 +0800

    blacklist ui

--- chat/kit/src/main/java/cn/wildfire/chat/kit/user/UserInfoActivity.java
+++ chat/kit/src/main/java/cn/wildfire/chat/kit/user/UserInfoActivity.java
@@ -10,8 +10,10 @@ import androidx.lifecycle.ViewModelProviders;
 import cn.wildfire.chat.app.main.MainActivity;
 import cn.wildfire.chat.kit.WfcBaseActivity;
 import cn.wildfire.chat.kit.contact.ContactViewModel;
+import cn.wildfire.chat.kit.contact.newfriend.InviteFriendActivity;
 import cn.wildfirechat.chat.R;
 import cn.wildfirechat.model.UserInfo;
+import cn.wildfirechat.remote.ChatManager;
 
 public class UserInfoActivity extends WfcBaseActivity {
     private UserInfo userInfo;
@@ -42,17 +44,64 @@ public class UserInfoActivity extends WfcBaseActivity {
     protected void afterMenus(Menu menu) {
         super.afterMenus(menu);
         ContactViewModel contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
-        if (!contactViewModel.isFriend(userInfo.uid)) {
-            MenuItem item = menu.findItem(R.id.delete);
-            item.setEnabled(false);
-            item.setVisible(false);
+
+        MenuItem itemDelete = menu.findItem(R.id.delete);
+        MenuItem itemAddFriend = menu.findItem(R.id.addFriend);
+        MenuItem itemAddBlacklist = menu.findItem(R.id.addBlacklist);
+        MenuItem itemRemoveBlacklist = menu.findItem(R.id.removeBlacklist);
+        MenuItem itemSetAlias = menu.findItem(R.id.setAlias);
+
+        if (ChatManager.Instance().getUserId().equals(userInfo.uid)) {
+            itemAddBlacklist.setEnabled(false);
+            itemAddBlacklist.setVisible(false);
+            itemAddFriend.setEnabled(false);
+            itemAddFriend.setVisible(false);
+            itemDelete.setEnabled(false);
+            itemDelete.setVisible(false);
+            itemRemoveBlacklist.setEnabled(false);
+            itemRemoveBlacklist.setVisible(false);
+            itemSetAlias.setEnabled(false);
+            itemSetAlias.setVisible(false);
+        } else {
+            if (contactViewModel.isFriend(userInfo.uid)) {
+                itemAddFriend.setEnabled(false);
+                itemAddFriend.setVisible(false);
+                itemDelete.setEnabled(true);
+                itemDelete.setVisible(true);
+                itemSetAlias.setEnabled(true);
+                itemSetAlias.setVisible(true);
+            } else {
+                itemAddFriend.setEnabled(true);
+                itemAddFriend.setVisible(true);
+                itemDelete.setEnabled(false);
+                itemDelete.setVisible(false);
+            }
+
+            if (contactViewModel.isBlacklisted(userInfo.uid)) {
+                itemAddBlacklist.setEnabled(false);
+                itemAddBlacklist.setVisible(false);
+                itemRemoveBlacklist.setEnabled(true);
+                itemRemoveBlacklist.setVisible(true);
+            } else {
+                itemAddBlacklist.setEnabled(true);
+                itemAddBlacklist.setVisible(true);
+                itemRemoveBlacklist.setEnabled(false);
+                itemRemoveBlacklist.setVisible(false);
+            }
         }
     }
 
+//    android:id="@+id/delete"
+//    android:id="@+id/addFriend"
+//    android:id="@+id/addBlacklist"
+//    android:id="@+id/removeBlacklist"
+//    android:id="@+id/setAlias"
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        ContactViewModel contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
+
         if (item.getItemId() == R.id.delete) {
-            ContactViewModel contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
             contactViewModel.deleteFriend(userInfo.uid).observe(
                     this, booleanOperateResult -> {
                         if (booleanOperateResult.isSuccess()) {
@@ -64,7 +113,40 @@ public class UserInfoActivity extends WfcBaseActivity {
                     }
             );
             return true;
+        } else if (item.getItemId() == R.id.addFriend) {
+            Intent intent = new Intent(this, InviteFriendActivity.class);
+            intent.putExtra("userInfo", userInfo);
+            startActivity(intent);
+            return true;
+        } else if (item.getItemId() == R.id.addBlacklist) {
+            contactViewModel.setBlacklist(userInfo.uid, true).observe(
+                    this, booleanOperateResult -> {
+                        if (booleanOperateResult.isSuccess()) {
+                            Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "add blacklist error " + booleanOperateResult.getErrorCode(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+            return true;
+        } else if (item.getItemId() == R.id.removeBlacklist) {
+            contactViewModel.setBlacklist(userInfo.uid, false).observe(
+                    this, booleanOperateResult -> {
+                        if (booleanOperateResult.isSuccess()) {
+                            Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "remove blacklist error " + booleanOperateResult.getErrorCode(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+            return true;
+        } else if (item.getItemId() == R.id.setAlias) {
+            Intent intent = new Intent(this, SetAliasActivity.class);
+            intent.putExtra("userId", userInfo.uid);
+            startActivity(intent);
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
