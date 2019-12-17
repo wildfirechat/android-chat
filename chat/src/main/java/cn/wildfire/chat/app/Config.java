fe93f12f36f04456f54e8399f153adda421a18d0
x86 <1960867973@qq.com>
2019-12-17 14:13:47 +0800

    update config

--- chat/src/main/java/cn/wildfire/chat/app/Config.java
+++ chat/src/main/java/cn/wildfire/chat/app/Config.java
@@ -13,13 +13,13 @@ public interface Config {
     // 仅仅是host，没有http开头，不可配置为127.0.0.1 或者 192.168.0.1
     // host可以是IP，可以是域名，如果是域名的话只支持主域名或www域名，二级域名不支持！
     // 例如：example.com或www.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。
-    String IM_SERVER_HOST = "192.168.31.113";
+    String IM_SERVER_HOST = "wildfirechat.cn";
 
     //客户端强制使用80端口，不能使用其它端口。需要确保服务器运行在在国内时处于备案状态，确保运营IM服务处在监管之下。
     //int IM_SERVER_PORT = 80;
 
     //正式商用时，建议用https，确保token安全
-    String APP_SERVER_ADDRESS = "http://192.168.31.113:9888";
+    String APP_SERVER_ADDRESS = "http://wildfirechat.cn:8888";
 
     String ICE_ADDRESS = "turn:turn.wildfirechat.cn:3478";
     String ICE_USERNAME = "wfchat";
