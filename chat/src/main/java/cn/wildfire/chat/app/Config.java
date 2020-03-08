9b5cb4841f714f0a58e61b6e3dc94349229cbd42
heavyrain.lee <heavyrain.lee@gmail.com>
2020-03-08 08:38:13 +0800

    添加注释

--- chat/src/main/java/cn/wildfire/chat/app/Config.java
+++ chat/src/main/java/cn/wildfire/chat/app/Config.java
@@ -25,7 +25,7 @@ public interface Config {
     String ICE_USERNAME = "wfchat";
     String ICE_PASSWORD = "wfchat";
 
-    // 二次开发时，一定记得替换为你们自己的
+    // 二次开发时，一定记得替换为你们自己的，ID请从BUGLY官网申请
     String BUGLY_ID = "34490ba79f";
 
     int DEFAULT_MAX_AUDIO_RECORD_TIME_SECOND = 120;
