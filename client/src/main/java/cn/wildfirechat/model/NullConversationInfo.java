c2ed9d0be5b302187277483dd0cde4cab811eb35
x86 <1960867973@qq.com>
2020-03-26 15:36:45 +0800

    add nullConversationInfo. fix #292

---
+++ client/src/main/java/cn/wildfirechat/model/NullConversationInfo.java
@@ -0,0 +1,8 @@
+package cn.wildfirechat.model;
+
+public class NullConversationInfo extends ConversationInfo {
+    public NullConversationInfo(Conversation conversation) {
+        super();
+        this.conversation = conversation;
+    }
+}
