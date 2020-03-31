88d2eb46b9869068bcd4a696803919b9ebc23274
heavyrain.lee <heavyrain.lee@gmail.com>
2020-03-31 08:20:09 +0800

    update comment

--- chat/src/main/java/cn/wildfire/chat/app/Config.java
+++ chat/src/main/java/cn/wildfire/chat/app/Config.java
@@ -11,14 +11,12 @@ import android.util.Log;
 public interface Config {
 
     // 仅仅是host，没有http开头，不可配置为127.0.0.1 或者 192.168.0.1
-    //可以是IP，可以是域名，如果是域名的话只支持主域名或www域名或im的二级域名，其它二级域名不支持！
-    //例如：example.com或www.example.com或im.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。
+    // 可以是IP，可以是域名，如果是域名的话只支持主域名或www域名或im的二级域名，其它二级域名不支持！建议使用域名。
+    // 例如：example.com或www.example.com或im.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。
     String IM_SERVER_HOST = "wildfirechat.cn";
 
-    //客户端强制使用80端口，不能使用其它端口。需要确保服务器运行在在国内时处于备案状态，确保运营IM服务处在监管之下。
-    //int IM_SERVER_PORT = 80;
-
-    //正式商用时，建议用https，确保token安全
+    // App Server默认使用的是8888端口，替换为自己部署的服务时需要注意端口别填错了
+    // 正式商用时，建议用https，确保token安全
     String APP_SERVER_ADDRESS = "http://wildfirechat.cn:8888";
 
     String ICE_ADDRESS = "turn:turn.wildfirechat.cn:3478";
