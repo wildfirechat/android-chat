42bbffbfd38dd91d7bea837c38cdb9d9a2b94b54
imndx <imndxx@gmail.com>
2020-04-06 16:46:39 +0800

    消息撤回重新编辑

--- client/src/main/java/cn/wildfirechat/message/notification/RecallMessageContent.java
+++ client/src/main/java/cn/wildfirechat/message/notification/RecallMessageContent.java
@@ -1,6 +1,10 @@
 package cn.wildfirechat.message.notification;
 
 import android.os.Parcel;
+import android.text.TextUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import cn.wildfirechat.message.Message;
 import cn.wildfirechat.message.core.ContentTag;
@@ -19,6 +23,13 @@ import static cn.wildfirechat.message.core.MessageContentType.ContentType_Recall
 public class RecallMessageContent extends NotificationMessageContent {
     private String operatorId;
     private long messageUid;
+
+    private String originalSender;
+    private int originalContentType;
+    private String originalSearchableContent;
+    private String originalContent;
+    private String originalExtra;
+    private long originalMessageTimestamp;
 
     public RecallMessageContent() {
     }
@@ -41,6 +52,20 @@ public class RecallMessageContent extends NotificationMessageContent {
     public void decode(MessagePayload payload) {
         operatorId = payload.content;
         messageUid = Long.parseLong(new String(payload.binaryContent));
+
+        if (!TextUtils.isEmpty(payload.extra)) {
+            try {
+                JSONObject obj = new JSONObject(payload.extra);
+                this.originalSender = obj.optString("s");
+                this.originalContentType = obj.optInt("t");
+                this.originalSearchableContent = obj.optString("sc");
+                this.originalContent = obj.optString("c");
+                this.originalExtra = obj.optString("e");
+                this.originalMessageTimestamp = obj.optLong("ts");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public String getOperatorId() {
@@ -49,6 +74,30 @@ public class RecallMessageContent extends NotificationMessageContent {
 
     public void setOperatorId(String operatorId) {
         this.operatorId = operatorId;
+    }
+
+    public String getOriginalSender() {
+        return originalSender;
+    }
+
+    public int getOriginalContentType() {
+        return originalContentType;
+    }
+
+    public String getOriginalSearchableContent() {
+        return originalSearchableContent;
+    }
+
+    public String getOriginalContent() {
+        return originalContent;
+    }
+
+    public String getOriginalExtra() {
+        return originalExtra;
+    }
+
+    public long getOriginalMessageTimestamp() {
+        return originalMessageTimestamp;
     }
 
     public long getMessageUid() {
@@ -76,6 +125,7 @@ public class RecallMessageContent extends NotificationMessageContent {
         return notification;
     }
 
+
     @Override
     public int describeContents() {
         return 0;
@@ -86,12 +136,24 @@ public class RecallMessageContent extends NotificationMessageContent {
         super.writeToParcel(dest, flags);
         dest.writeString(this.operatorId);
         dest.writeLong(this.messageUid);
+        dest.writeString(this.originalSender);
+        dest.writeInt(this.originalContentType);
+        dest.writeString(this.originalSearchableContent);
+        dest.writeString(this.originalContent);
+        dest.writeString(this.originalExtra);
+        dest.writeLong(this.originalMessageTimestamp);
     }
 
     protected RecallMessageContent(Parcel in) {
         super(in);
         this.operatorId = in.readString();
         this.messageUid = in.readLong();
+        this.originalSender = in.readString();
+        this.originalContentType = in.readInt();
+        this.originalSearchableContent = in.readString();
+        this.originalContent = in.readString();
+        this.originalExtra = in.readString();
+        this.originalMessageTimestamp = in.readLong();
     }
 
     public static final Creator<RecallMessageContent> CREATOR = new Creator<RecallMessageContent>() {
