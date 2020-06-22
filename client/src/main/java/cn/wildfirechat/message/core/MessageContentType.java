27141278b61437f59e57c0310bf317eac9470478
heavyrain2012 <heavyrain.lee@gmail.com>
2020-06-22 14:19:43 +0800

    fix build error

--- client/src/main/java/cn/wildfirechat/message/core/MessageContentType.java
+++ client/src/main/java/cn/wildfirechat/message/core/MessageContentType.java
@@ -21,7 +21,7 @@ public interface MessageContentType {
 
     int ContentType_Recall = 80;
     //删除消息，请勿直接发送此消息，此消息是服务器端删除时的同步消息
-    int ContentType_Delete = 81
+    int ContentType_Delete = 81;
 
     //提醒消息
     int ContentType_Tip_Notification = 90;
