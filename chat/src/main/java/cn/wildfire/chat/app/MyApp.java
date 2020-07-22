dc15a8a1549d7c3dfb893cd9d6cc302a63fe43ef
imndx <imndxx@gmail.com>
2020-07-22 11:07:08 +0800

    update config

--- chat/src/main/java/cn/wildfire/chat/app/MyApp.java
+++ chat/src/main/java/cn/wildfire/chat/app/MyApp.java
@@ -18,6 +18,10 @@ import cn.wildfirechat.push.PushService;
 
 public class MyApp extends BaseApp {
 
+
+    // 一定记得替换为你们自己的，ID请从BUGLY官网申请。关于BUGLY，可以从BUGLY官网了解，或者百度。
+    public static String BUGLY_ID = "34490ba79f";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -25,10 +29,12 @@ public class MyApp extends BaseApp {
 
         // bugly，务必替换为你自己的!!!
         if ("wildfirechat.cn".equals(Config.IM_SERVER_HOST)) {
-            CrashReport.initCrashReport(getApplicationContext(), Config.BUGLY_ID, false);
+            CrashReport.initCrashReport(getApplicationContext(), BUGLY_ID, false);
         }
         // 只在主进程初始化，否则会导致重复收到消息
         if (getCurProcessName(this).equals(BuildConfig.APPLICATION_ID)) {
+            // 如果uikit是以aar的方式引入 ，那么需要在此对Config里面的属性进行配置，如：
+            // Config.IM_SERVER_HOST = "im.example.com";
             WfcUIKit wfcUIKit = WfcUIKit.getWfcUIKit();
             wfcUIKit.init(this);
             wfcUIKit.setAppServiceProvider(AppService.Instance());
@@ -62,10 +68,10 @@ public class MyApp extends BaseApp {
         int pid = android.os.Process.myPid();
 
         ActivityManager activityManager = (ActivityManager) context
-                .getSystemService(Context.ACTIVITY_SERVICE);
+            .getSystemService(Context.ACTIVITY_SERVICE);
 
         for (ActivityManager.RunningAppProcessInfo appProcess : activityManager
-                .getRunningAppProcesses()) {
+            .getRunningAppProcesses()) {
 
             if (appProcess.pid == pid) {
                 return appProcess.processName;
