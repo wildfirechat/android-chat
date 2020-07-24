5f342df0c41e81ae71b762261d8420f1485ddee5
imndx <imndxx@gmail.com>
2020-07-24 11:14:41 +0800

    fix onFriendListUpdate issue. fix #356

--- client/src/main/java/cn/wildfirechat/remote/OnFriendUpdateListener.java
+++ client/src/main/java/cn/wildfirechat/remote/OnFriendUpdateListener.java
@@ -3,6 +3,10 @@ package cn.wildfirechat.remote;
 import java.util.List;
 
 public interface OnFriendUpdateListener {
+    /**
+     * 好友列表更新回调
+     * @param updateFriendList 只包含有更新的好友，不包含被删除的好友，所以这个回调的时候，需要调用一下{@link ChatManager#getMyFriendList(boolean)}获取当前完整的好友列表
+     */
     void onFriendListUpdate(List<String> updateFriendList);
 
     void onFriendRequestUpdate();
