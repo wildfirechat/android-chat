9c5eb128fb668a5d6a4e865788c9754b83af0deb
imndx <imndxx@gmail.com>
2020-07-28 16:23:49 +0800

    update

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -2,6 +2,7 @@ package cn.wildfire.chat.kit;
 
 import android.os.Environment;
 import android.text.TextUtils;
+import android.util.Log;
 
 /**
  * Created by heavyrain lee on 2017/11/24.
@@ -9,10 +10,10 @@ import android.text.TextUtils;
 
 public class Config {
 
-    // 仅仅是host，没有http开头，不可配置为127.0.0.1 或者 192.168.0.1
+    // 仅仅是host，没有http开头，也不用配置端口，底层会使用默认的80端口，不可配置为127.0.0.1 或者 192.168.0.1
     // 可以是IP，可以是域名，如果是域名的话只支持主域名或www域名或im的二级域名，其它二级域名不支持！建议使用域名。
     // 例如：example.com或www.example.com或im.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。
-    public static String IM_SERVER_HOST = "wildfirechat.cn";
+    public static String IM_SERVER_HOST = "wildfirechat.cn"; // 会固定使用到80端口，不可以配置端口!!!
 
     // App Server默认使用的是8888端口，替换为自己部署的服务时需要注意端口别填错了
     // 正式商用时，建议用https，确保token安全
@@ -45,5 +46,8 @@ public class Config {
             throw new IllegalStateException("im server host config error");
         }
 
+        if(Config.IM_SERVER_HOST.contains(":")){
+            Log.e("WFC", "!!!!IM_SERVER_HOST不能配置端口!!!!");
+        }
     }
 }
