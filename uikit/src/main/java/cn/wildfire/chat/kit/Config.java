4710e6577d62ca6f0ed21cfe160714d55eb3ff79
x86 <1960867973@qq.com>
2020-08-08 11:26:31 +0800

    refactor

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -1,16 +1,14 @@
 package cn.wildfire.chat.kit;
 
 import android.os.Environment;
-import android.text.TextUtils;
 
 /**
  * Created by heavyrain lee on 2017/11/24.
  */
 
-public class Config {
+public class Config<ICE_SERVERS> {
 
     /**
-     *
      * 仅仅是host，没有http开头，也不用配置端口，<strong> 底层会使用默认的80端口</strong>，不可配置为127.0.0.1 或者 192.168.0.1
      * <br>
      * <br>
@@ -23,21 +21,15 @@ public class Config {
      */
     public static String IM_SERVER_HOST = "wildfirechat.cn";
 
-    /**
-     *
-     * App Server默认使用的是8888端口，替换为自己部署的服务时需要注意端口别填错了
-     * <br>
-     * <br>
-     * 正式商用时，建议用https，确保token安全
-     * <br>
-     * <br>
-     */
-    public static String APP_SERVER_ADDRESS = "http://wildfirechat.cn:8888";
+    // 注意APP_SERVER_ADDRESS已从kit中移除，移动到了AppService中
+    //public static String APP_SERVER_ADDRESS = "http://wildfirechat.cn:8888";
 
-    public static String ICE_ADDRESS = "turn:turn.wildfirechat.cn:3478";
-    public static String ICE_ADDRESS2 = "turn:117.51.153.82:3478";
-    public static String ICE_USERNAME = "wfchat";
-    public static String ICE_PASSWORD = "wfchat";
+    public static String[][] ICE_SERVERS = new String[][]{
+        // 数组元素定义
+        /*{"turn server uri", "userName", "password"}*/
+        {"turn:turn.wildfirechat.cn:3478", "wfchat", "wfchat"},
+        {"turn:117.51.153.82:3478", "wfchat", "wfchat"}
+    };
 
     public static int DEFAULT_MAX_AUDIO_RECORD_TIME_SECOND = 120;
 
@@ -50,19 +42,4 @@ public class Config {
     public static String PHOTO_SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/wfc/photo";
     public static String FILE_SAVE_DIR = Environment.getExternalStorageDirectory().getPath() + "/wfc/file";
 
-    public static void validateConfig() {
-        if (TextUtils.isEmpty(IM_SERVER_HOST)
-            || IM_SERVER_HOST.startsWith("http")
-            || IM_SERVER_HOST.contains(":")
-            || TextUtils.isEmpty(APP_SERVER_ADDRESS)
-            || (!APP_SERVER_ADDRESS.startsWith("http") && !APP_SERVER_ADDRESS.startsWith("https"))
-            || IM_SERVER_HOST.equals("127.0.0.1")
-            || APP_SERVER_ADDRESS.contains("127.0.0.1")
-            || (!IM_SERVER_HOST.contains("wildfirechat.cn") && APP_SERVER_ADDRESS.contains("wildfirechat.cn"))
-            || (IM_SERVER_HOST.contains("wildfirechat.cn") && !APP_SERVER_ADDRESS.contains("wildfirechat.cn"))
-            || !ICE_ADDRESS.startsWith("turn")
-        ) {
-            throw new IllegalArgumentException("config error\n 参数配置错误\n请仔细阅读Config.java中的注释，并检查配置!\n");
-        }
-    }
 }
