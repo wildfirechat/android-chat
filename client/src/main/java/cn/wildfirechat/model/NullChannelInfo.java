6b5c74cf2695afd9b9aaa3bb51ae666f8dd4aeff
imndx <imndxx@gmail.com>
2020-08-11 21:22:24 +0800

    update defalut user/group/channel/chatroom name

--- client/src/main/java/cn/wildfirechat/model/NullChannelInfo.java
+++ client/src/main/java/cn/wildfirechat/model/NullChannelInfo.java
@@ -3,7 +3,8 @@ package cn.wildfirechat.model;
 public class NullChannelInfo extends ChannelInfo {
     public NullChannelInfo(String channelId) {
         this.channelId = channelId;
-        this.name = "<" + channelId + ">";
+        //this.name = "<" + channelId + ">";
+        this.name = "频道";
         this.owner = "";
     }
 }
