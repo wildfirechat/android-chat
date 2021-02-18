dcabcd6d0bc3c0b58fd16554420cffcc5fb65b51
heavyrain.lee <heavyrain.lee@gmail.com>
2021-02-18 16:07:42 +0800

    切换到net域名

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -23,7 +23,7 @@ public class Config {
      * <br>
      * <br>
      */
-    public static String IM_SERVER_HOST /*请仔细阅读上面的注释*/ = "wildfirechat.cn";
+    public static String IM_SERVER_HOST /*请仔细阅读上面的注释*/ = "wildfirechat.net";
 
     // 注意APP_SERVER_ADDRESS已从kit中移除，移动到了AppService.java中
     //public static String APP_SERVER_ADDRESS = "http://wildfirechat.cn:8888";
@@ -41,7 +41,7 @@ public class Config {
         // 如果是高级版，请删除掉下面的配置项目，保持ICE_SERVERS为空数组就行。
         // 数组元素定义
         /*{"turn server uri", "userName", "password"}*/
-        {"turn:turn.wildfirechat.cn:3478", "wfchat", "wfchat"}
+        {"turn:turn.wildfirechat.net:3478", "wfchat", "wfchat"}
     };
 
     //文件传输助手用户ID，服务器有个默认文件助手的机器人，如果修改它的ID，需要客户端和服务器数据库同步修改
