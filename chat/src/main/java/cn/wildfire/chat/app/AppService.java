2ff6b6e9eff448c600b88ab712683e624b9401d5
heavyrain.lee <heavyrain.lee@gmail.com>
2021-02-19 17:03:50 +0800

    切换回cn域名

--- chat/src/main/java/cn/wildfire/chat/app/AppService.java
+++ chat/src/main/java/cn/wildfire/chat/app/AppService.java
@@ -49,7 +49,7 @@ public class AppService implements AppServiceProvider {
      * <br>
      * <br>
      */
-    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "http://wildfirechat.net:8888";
+    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "http://wildfirechat.cn:8888";
 //    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "https://app.wildfirechat.net";
 
     private AppService() {
