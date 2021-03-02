a7644afaa1a69436163a5b1abe621dc3abf0fed1
x86 <1960867973@qq.com>
2021-03-02 21:01:53 +0800

    解决音视频会议悬浮窗问题

--- uikit/src/main/java/cn/wildfire/chat/kit/voip/VoipDummyActivity.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/voip/VoipDummyActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import cn.wildfire.chat.kit.voip.conference.ConferenceActivity;
 import cn.wildfirechat.avenginekit.AVEngineKit;
 import cn.wildfirechat.model.Conversation;
 
@@ -18,18 +19,26 @@ public class VoipDummyActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AVEngineKit.CallSession session = AVEngineKit.Instance().getCurrentSession();
-        if (session == null || session.getState() == AVEngineKit.CallState.Idle || session.getConversation() == null) {
+        if (session == null || session.getState() == AVEngineKit.CallState.Idle) {
+            finish();
+        } else if (session.isConference()) {
+            Intent intent = new Intent(this, ConferenceActivity.class);
+            startActivity(intent);
             finish();
         } else {
+            if (session.getConversation() == null) {
+                finish();
+                return;
+            }
             Intent intent;
             if (session.getConversation().type == Conversation.ConversationType.Single) {
                 intent = new Intent(this, SingleCallActivity.class);
             } else {
                 intent = new Intent(this, MultiCallActivity.class);
             }
-            finish();
             startActivity(intent);
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
         }
     }
 }
