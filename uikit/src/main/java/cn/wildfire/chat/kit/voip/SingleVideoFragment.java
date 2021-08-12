d4fbb14b423ecfa6ff87c138b497f350b9ec3c48
imndx <imndxx@gmail.com>
2021-08-12 21:16:50 +0800

    update avengine

--- uikit/src/main/java/cn/wildfire/chat/kit/voip/SingleVideoFragment.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/voip/SingleVideoFragment.java
@@ -227,6 +227,10 @@ public class SingleVideoFragment extends Fragment implements AVEngineKit.CallSes
 
     @OnClick(R2.id.shareScreenImageView)
     void shareScreen() {
+        if(!AVEngineKit.isSupportConference() && !AVEngineKit.isSupportMultiCall()){
+            Toast.makeText(getActivity(), "该版本不支持屏幕共享", Toast.LENGTH_SHORT).show();
+            return;
+        }
         AVEngineKit.CallSession session = gEngineKit.getCurrentSession();
         if (session == null || session.getState() != AVEngineKit.CallState.Connected) {
             return;
