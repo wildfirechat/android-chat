6451a3fa9c6a3ed90c1be912e365e2297d6d48eb
imndx <imndxx@gmail.com>
2021-12-10 11:00:54 +0800

    搜索列表，群组头像显示异常

--- uikit/src/main/java/cn/wildfire/chat/kit/search/viewHolder/GroupViewHolder.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/search/viewHolder/GroupViewHolder.java
@@ -4,17 +4,23 @@
 
 package cn.wildfire.chat.kit.search.viewHolder;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.wildfire.chat.kit.GlideApp;
+import cn.wildfire.chat.kit.R;
 import cn.wildfire.chat.kit.R2;
+import cn.wildfire.chat.kit.third.utils.ImageUtils;
+import cn.wildfire.chat.kit.third.utils.UIUtils;
 import cn.wildfirechat.model.GroupSearchResult;
 
 public class GroupViewHolder extends ResultItemViewHolder<GroupSearchResult> {
@@ -34,7 +40,16 @@ public class GroupViewHolder extends ResultItemViewHolder<GroupSearchResult> {
     @Override
     public void onBind(String keyword, GroupSearchResult groupSearchResult) {
         nameTextView.setText(groupSearchResult.groupInfo.name);
-        Glide.with(fragment).load(groupSearchResult.groupInfo.portrait).into(portraitImageView);
+        String portrait = groupSearchResult.groupInfo.portrait;
+        if (TextUtils.isEmpty(portrait)) {
+            portrait = ImageUtils.getGroupGridPortrait(fragment.getContext(), groupSearchResult.groupInfo.target, 60);
+        }
+        GlideApp
+            .with(fragment)
+            .load(portrait)
+            .placeholder(R.mipmap.ic_group_cheat)
+            .transforms(new CenterCrop(), new RoundedCorners(UIUtils.dip2Px(fragment.getContext(), 4)))
+            .into(portraitImageView);
 
         String desc = "";
         switch (groupSearchResult.marchedType) {
