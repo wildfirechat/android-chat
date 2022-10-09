64b6ce0ef2ae8c28776fcf1e674fccec591b2466
imndx <imndxx@gmail.com>
2022-10-10 00:09:27 +0800

    会议布局重构

---
+++ uikit/src/main/java/cn/wildfire/chat/kit/widget/ClickableViewPager.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 WildFireChat. All rights reserved.
+ */
+
+package cn.wildfire.chat.kit.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.viewpager.widget.ViewPager;
+
+public class ClickableViewPager extends ViewPager {
+
+    private OnClickListener mOnClickListener;
+
+    public ClickableViewPager(Context context) {
+        super(context);
+
+        setup();
+    }
+
+    public ClickableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setup();
+    }
+
+    private void setup() {
+        final GestureDetector tapGestureDetector = new GestureDetector(getContext(), new TapGestureListener());
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                tapGestureDetector.onTouchEvent(event);
+
+                return false;
+            }
+        });
+    }
+
+    public void setOnViewPagerClickListener(OnClickListener onClickListener) {
+        mOnClickListener = onClickListener;
+    }
+
+    public interface OnClickListener {
+        void onViewPagerClick(ViewPager viewPager);
+    }
+
+    private class TapGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            if (mOnClickListener != null) {
+                mOnClickListener.onViewPagerClick(ClickableViewPager.this);
+            }
+
+            return true;
+        }
+    }
+}
