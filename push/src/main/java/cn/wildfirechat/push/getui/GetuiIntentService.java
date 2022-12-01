4decd23c9f84c346774f2faa9a9d09404ae234f0
imndx <imndxx@gmail.com>
2022-12-01 12:09:12 +0800

    个推设置 deviceToken

--- push/src/main/java/cn/wildfirechat/push/getui/GetuiIntentService.java
+++ push/src/main/java/cn/wildfirechat/push/getui/GetuiIntentService.java
@@ -9,11 +9,15 @@ import android.util.Log;
 
 import com.igexin.sdk.GTIntentService;
 
+import cn.wildfirechat.push.PushService;
+import cn.wildfirechat.remote.ChatManager;
+
 public class GetuiIntentService extends GTIntentService {
     private static final String TAG = "Getui";
 
     @Override
     public void onReceiveClientId(Context context, String s) {
         Log.d(TAG, "onReceiveClientId: " + s);
+        ChatManager.Instance().setDeviceToken(s, PushService.PushServiceType.Getui.ordinal());
     }
 }
