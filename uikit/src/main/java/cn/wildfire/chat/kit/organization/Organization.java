e118bc51b44d2039b5b65cc410fb0060b2b88f08
imndx <imndxx@gmail.com>
2023-01-05 21:58:34 +0800

    添加组织结构相关pojo等

---
+++ uikit/src/main/java/cn/wildfire/chat/kit/organization/Organization.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023 WildFireChat. All rights reserved.
+ */
+
+package cn.wildfire.chat.kit.organization;
+
+public class Organization {
+    public String organizationId;
+    public int parentId;
+    public String managerId;
+    public String name;
+    public String desc;
+    public String portraitUrl;
+    public String tel;
+    public String office;
+    public String groupId;
+    public int memberCount;
+    public int sort;
+    public long updateDt;
+    public long createDt;
+
+}
---
+++ uikit/src/main/java/cn/wildfire/chat/kit/organization/OrganizationServiceProvider.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 WildFireChat. All rights reserved.
+ */
+
+package cn.wildfire.chat.kit.organization;
+
+import java.util.List;
+
+import cn.wildfire.chat.kit.net.SimpleCallback;
+
+public interface OrganizationServiceProvider {
+    void getRelationship(String employeeId, SimpleCallback<OrganizationRelationship> callback);
+
+    void getRootOrganization(SimpleCallback<OrganizationRelationship> callback);
+
+    void getOrganizationEx(int orgId, SimpleCallback<OrganizationEx> callback);
+
+    void getOrganizations(List<Integer> orgIds, SimpleCallback<List<Organization>> callback);
+
+    void batchGetOrgEmployees(List<Integer> orgIds, SimpleCallback<List<String>> callback);
+
+    void getOrgEmployees(int orgId, SimpleCallback<List<String>> callback);
+
+    void getEmployee(String employeeId, SimpleCallback<Employee> callback);
+
+    void getEmployeeEx(String employeeId, SimpleCallback<EmployeeEx> callback);
+
+    void searchEmployee(int orgId, String keyword, SimpleCallback<List<Employee>> callback);
+
+}
