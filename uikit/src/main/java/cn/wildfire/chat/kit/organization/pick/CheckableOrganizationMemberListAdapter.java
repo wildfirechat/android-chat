14d739b6b50d7fccd15994bfcf984db6dc7e41cb
imndx <imndxx@gmail.com>
2023-01-14 17:10:41 +0800

    fix build error

--- uikit/src/main/java/cn/wildfire/chat/kit/organization/pick/CheckableOrganizationMemberListAdapter.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/organization/pick/CheckableOrganizationMemberListAdapter.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2023 WildFireChat. All rights reserved.
+ */
+
 package cn.wildfire.chat.kit.organization.pick;
-   class CheckableOrganizationMemberListAdapter   {
+
+public class CheckableOrganizationMemberListAdapter {
 }
