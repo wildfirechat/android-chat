c1fc69b18b688fd7ae18a3a3019742055194152e
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-03-31 09:56:38 +0800

    Revert "Revert "super group""
    This reverts commit 766be5aeb98e2556c18f2fe4f75f5f3d9d70b4e4.

--- client/src/main/java/cn/wildfirechat/remote/ChatManager.java
+++ client/src/main/java/cn/wildfirechat/remote/ChatManager.java
@@ -3683,6 +3683,31 @@ public class ChatManager {
     }
 
     /**
+     * 清除messageId之前（包含）的消息未读状态
+     * @param messageId
+     * @param conversation
+     */
+    public void clearUnreadStatusBeforeMessage(long messageId, Conversation conversation) {
+        if (!checkRemoteService()) {
+            return;
+        }
+
+        try {
+            Message msg = getMessage(messageId);
+            if (msg != null) {
+                if (mClient.clearUnreadStatusBeforeMessage(messageId, conversation)) {
+                    ConversationInfo conversationInfo = getConversation(msg.conversation);
+                    for (OnConversationInfoUpdateListener listener : conversationInfoUpdateListeners) {
+                        listener.onConversationUnreadStatusClear(conversationInfo);
+                    }
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * 设置audio获取media等媒体消息的播放状态(其实是可以设置所有类型的消息为已播放，可以根据业务需求来处理)
      *
      * @param messageId
