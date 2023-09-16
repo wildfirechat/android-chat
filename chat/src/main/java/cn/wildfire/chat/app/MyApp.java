b634522dc66a622521a1a08d9e2a8761ee490dff
imndx <imndxx@gmail.com>
2023-09-16 16:51:24 +0800

    添加诊断页面

--- chat/src/main/java/cn/wildfire/chat/app/MyApp.java
+++ chat/src/main/java/cn/wildfire/chat/app/MyApp.java
@@ -20,11 +20,18 @@ import cn.wildfirechat.chat.BuildConfig;
 import cn.wildfirechat.chat.R;
 import cn.wildfirechat.push.PushService;
 import cn.wildfirechat.remote.ChatManager;
+import cn.wildfirechat.remote.OnConnectToServerListener;
 
-public class MyApp extends BaseApp {
+public class MyApp extends BaseApp implements OnConnectToServerListener {
 
     // 一定记得替换为你们自己的，ID请从BUGLY官网申请。关于BUGLY，可以从BUGLY官网了解，或者百度。
     public static String BUGLY_ID = "15dfd5f6d1";
+
+    public static String routeHost;
+    public static int routePort;
+
+    public static String longLinkHost;
+    public static int longLinkPort;
 
     @Override
     public void onCreate() {
@@ -61,6 +68,7 @@ public class MyApp extends BaseApp {
             }
 
             ChatManager.Instance().setDefaultPortraitProviderClazz(WfcDefaultPortraitProvider.class);
+            ChatManager.Instance().addConnectToServerListener(this);
 
             Config.ENABLE_AUDIO_MESSAGE_AMPLIFICATION = sp.getBoolean("audioMessageAmplificationEnabled", false);
         }
@@ -94,5 +102,17 @@ public class MyApp extends BaseApp {
             }
         }
         return null;
+    }
+
+
+    @Override
+    public void onConnectToServer(String host, String ip, int port) {
+        if (TextUtils.isEmpty(ip)) {
+            routeHost = host;
+            routePort = port;
+        } else {
+            longLinkHost = host;
+            longLinkPort = port;
+        }
     }
 }
---
+++ chat/src/main/java/cn/wildfire/chat/app/misc/DiagnoseActivity.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2023 WildFireChat. All rights reserved.
+ */
+
+package cn.wildfire.chat.app.misc;
+
+import android.text.TextUtils;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import cn.wildfire.chat.app.AppService;
+import cn.wildfire.chat.app.MyApp;
+import cn.wildfire.chat.kit.Config;
+import cn.wildfire.chat.kit.WfcBaseActivity;
+import cn.wildfire.chat.kit.net.OKHttpHelper;
+import cn.wildfire.chat.kit.net.SimpleCallback;
+import cn.wildfirechat.avenginekit.AVEngineKit;
+import cn.wildfirechat.chat.R;
+import cn.wildfirechat.remote.ChatManager;
+
+public class DiagnoseActivity extends WfcBaseActivity {
+
+    private TextView configInfoTextView;
+    private TextView diagnoseResultTextView;
+
+    private StringBuffer diagnoseResultSB;
+
+    @Override
+    protected int contentLayout() {
+        return R.layout.activity_diagnose;
+    }
+
+    protected void bindEvents() {
+        super.bindEvents();
+        findViewById(R.id.startDiagnoseButton).setOnClickListener(v -> diagnose());
+    }
+
+    protected void bindViews() {
+        super.bindViews();
+        configInfoTextView = findViewById(R.id.configInfoTextView);
+        diagnoseResultTextView = findViewById(R.id.resultTextView);
+    }
+
+    @Override
+    protected void afterViews() {
+        super.afterViews();
+        updateConfigInfo();
+    }
+
+    private void diagnose() {
+        // 1. app-server
+        // 2. api/version
+        // 3. tcping
+
+        Toast.makeText(this, "开始进行网络诊断，请稍后", Toast.LENGTH_SHORT).show();
+
+        diagnoseResultSB = new StringBuffer();
+        checkAppServer();
+        checkApiVersion();
+        tcping();
+    }
+
+    private void updateConfigInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("APP-Sever: " + AppService.APP_SERVER_ADDRESS);
+        sb.append("\n");
+        sb.append("Route-Host: " + Config.IM_SERVER_HOST);
+        sb.append("\n");
+        sb.append("Route-Port: " + MyApp.routePort);
+        sb.append("\n");
+        sb.append("Long-Link-Port: " + MyApp.longLinkHost);
+        sb.append("\n");
+        sb.append("Long-Link-Port: " + MyApp.longLinkPort);
+        sb.append("\n");
+        sb.append("音视频 SDK: " + (AVEngineKit.isSupportConference() ? "高级版" : "多人版") + "\n");
+        String ices = "";
+        for (String[] ice : Config.ICE_SERVERS) {
+            ices += ice[0] + " " + ice[1] + " " + ice[2] + "\n";
+        }
+        sb.append("Turn-Server: " + ices);
+        sb.append("协议栈版本：" + ChatManager.Instance().getProtoRevision());
+        sb.append("\n");
+
+        configInfoTextView.setText(sb.toString());
+    }
+
+    private void updateDiagnoseResult() {
+        diagnoseResultTextView.setText(diagnoseResultSB.toString());
+    }
+
+    private void checkAppServer() {
+        OKHttpHelper.get(AppService.APP_SERVER_ADDRESS, null, new SimpleCallback<String>() {
+            @Override
+            public void onUiSuccess(String s) {
+                if ("Ok".equals(s)) {
+                    diagnoseResultSB.append("APP-Server 正常");
+                    diagnoseResultSB.append("\n\n");
+
+                    updateDiagnoseResult();
+                } else {
+                    diagnoseResultSB.append("APP-Server 异常: " + s);
+                    diagnoseResultSB.append("\n\n");
+                }
+            }
+
+            @Override
+            public void onUiFailure(int code, String msg) {
+                diagnoseResultSB.append("APP-Server 异常: " + code + " " + msg);
+                diagnoseResultSB.append("\n\n");
+
+                updateDiagnoseResult();
+            }
+        });
+    }
+
+    private void checkApiVersion() {
+        String url = "http://" + Config.IM_SERVER_HOST + ":" + MyApp.routePort + "/api/version";
+        OKHttpHelper.get(url, null, new SimpleCallback<String>() {
+            @Override
+            public void onUiSuccess(String s) {
+                try {
+                    JSONObject json = new JSONObject(s);
+                    diagnoseResultSB.append("IM-Server api/version 正常\n");
+                    diagnoseResultSB.append("remoteOriginUrl: " + json.getString("remoteOriginUrl"));
+                    diagnoseResultSB.append("\n");
+                    diagnoseResultSB.append("commitMessageShort: " + json.getString("commitMessageShort"));
+                    diagnoseResultSB.append("\n");
+                    diagnoseResultSB.append("commitTime: " + json.getString("commitTime"));
+                    diagnoseResultSB.append("\n\n");
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                updateDiagnoseResult();
+            }
+
+            @Override
+            public void onUiFailure(int code, String msg) {
+                diagnoseResultSB.append("IM-Server api/version 异常: " + code + " " + msg);
+                diagnoseResultSB.append("\n");
+
+                updateDiagnoseResult();
+            }
+        });
+    }
+
+    private void tcping() {
+        if (TextUtils.isEmpty(MyApp.longLinkHost)) {
+            Toast.makeText(this, "长连接地址为空，route 请求可能已异常", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String host = MyApp.longLinkHost;
+        int port = MyApp.longLinkPort;
+        ChatManager.Instance().getWorkHandler().post(() -> {
+            try (Socket socket = new Socket(host, port)) {
+                diagnoseResultSB.append("IM-Server 长连接 tcp ping 正常");
+                diagnoseResultSB.append("\n\n");
+
+                ChatManager.Instance().getMainHandler().post(this::updateDiagnoseResult);
+            } catch (IOException e) {
+                diagnoseResultSB.append("IM-Server 长连接 tcp ping 异常: " + e.getMessage());
+                diagnoseResultSB.append("\n\n");
+                ChatManager.Instance().getMainHandler().post(this::updateDiagnoseResult);
+            }
+        });
+    }
+}
