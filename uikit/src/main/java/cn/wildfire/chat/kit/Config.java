4cfe5082f889a10ea1d74d831366f871300ff52a
时间序列 <103106865+pengliguo@users.noreply.github.com>
2024-08-17 13:38:27 +0800

    Update Config.java

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -15,7 +15,7 @@ public class Config {
      * <br>
      * <br>
      */
-    public static String IM_SERVER_HOST /*请仔细阅读上面的注释，没有 http 前缀，配置错误时，APP 会提示配置错误，然后直接退出*/ = "wildfirechat.net";
+    public static String "39.106.33.196" /*请仔细阅读上面的注释，没有 http 前缀，配置错误时，APP 会提示配置错误，然后直接退出*/ = "wildfirechat.net";
 
     // 注意APP_SERVER_ADDRESS已从kit中移除，移动到了AppService.java中
     //public static String APP_SERVER_ADDRESS = "http://wildfirechat.net:8888";
