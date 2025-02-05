5c1b972d31874a82b4f1173f1482e3e7b92b3e54
imndx <imndxx@gmail.com>
2025-02-05 15:17:01 +0800

    fix build error

--- uikit/src/main/java/cn/wildfire/chat/kit/WfcNotificationManager.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/WfcNotificationManager.java
@@ -48,7 +48,7 @@ public class WfcNotificationManager {
 
     private static WfcNotificationManager notificationManager;
     // 小米系统上，允许推送时，默认还是没有铃声的。将 wfcChannelId 设置为接入推送时，在小米后台申请的 channelId，可以保证铃声生效
-    private static final String wfcChannelId = "wfc_notification";
+    private static final String wfcNotificationChannelId = "wfc_notification";
 
     private final List<Long> notificationMessages = new ArrayList<>();
     private int friendRequestNotificationId = 10000;
@@ -70,7 +70,7 @@ public class WfcNotificationManager {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Uri notificationRingUri = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.receive_msg_notification);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(wfcChannelId,
+            NotificationChannel channel = new NotificationChannel(wfcNotificationChannelId,
                 "野火IM 消息通知",
                 NotificationManager.IMPORTANCE_HIGH);
 
@@ -89,7 +89,7 @@ public class WfcNotificationManager {
         }
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, wfcNotificationChannelId)
             .setSmallIcon(R.mipmap.ic_launcher_notification)
             .setAutoCancel(true)
             .setCategory(CATEGORY_MESSAGE)
