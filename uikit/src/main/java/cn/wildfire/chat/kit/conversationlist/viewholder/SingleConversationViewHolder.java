132661930538410016ede9e0c560a32d45053340
imndx <imndxx@gmail.com>
2025-04-18 00:13:40 +0800

    fix memory leak

--- uikit/src/main/java/cn/wildfire/chat/kit/conversationlist/viewholder/SingleConversationViewHolder.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/conversationlist/viewholder/SingleConversationViewHolder.java
@@ -7,7 +7,8 @@ package cn.wildfire.chat.kit.conversationlist.viewholder;
 import android.view.View;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -22,30 +23,47 @@ import cn.wildfire.chat.kit.third.utils.UIUtils;
 import cn.wildfire.chat.kit.user.UserViewModel;
 import cn.wildfirechat.model.Conversation;
 import cn.wildfirechat.model.ConversationInfo;
+import cn.wildfirechat.model.UserInfo;
 
 @ConversationInfoType(type = Conversation.ConversationType.Single, line = 0)
 @EnableContextMenu
-public class SingleConversationViewHolder extends ConversationViewHolder {
+public class SingleConversationViewHolder extends ConversationViewHolder implements Observer<UserInfo> {
+    private LiveData<UserInfo> userInfoLiveData;
+    private UserViewModel userViewModel = null;
+
     public SingleConversationViewHolder(Fragment fragment, RecyclerView.Adapter adapter, View itemView) {
         super(fragment, adapter, itemView);
     }
 
     @Override
     protected void onBindConversationInfo(ConversationInfo conversationInfo) {
-        UserViewModel userViewModel = WfcUIKit.getAppScopeViewModel(UserViewModel.class);
-        userViewModel.getUserInfoAsync(conversationInfo.conversation.target, false)
-            .observe(fragment, userInfo -> {
-                CharSequence name = userViewModel.getUserDisplayNameEx(userInfo);
-                String portrait;
-                portrait = userInfo.portrait;
-                Glide
-                    .with(fragment)
-                    .load(portrait)
-                    .placeholder(R.mipmap.avatar_def)
-                    .transforms(new CenterCrop(), new RoundedCorners(UIUtils.dip2Px(fragment.getContext(), 4)))
-                    .into(portraitImageView);
-                nameTextView.setText(name);
-            });
+        if (userViewModel == null) {
+            userViewModel = WfcUIKit.getAppScopeViewModel(UserViewModel.class);
+        }
+        if (userInfoLiveData != null) {
+            userInfoLiveData.removeObserver(this);
+        }
+        userInfoLiveData = userViewModel.getUserInfoAsync(conversationInfo.conversation.target, false);
+        userInfoLiveData.observe(fragment, this);
     }
 
+    public void removeLiveDataObserver() {
+        if (userInfoLiveData != null) {
+            userInfoLiveData.removeObserver(this);
+        }
+    }
+
+    @Override
+    public void onChanged(UserInfo userInfo) {
+        CharSequence name = this.userViewModel.getUserDisplayNameEx(userInfo);
+        String portrait;
+        portrait = userInfo.portrait;
+        Glide
+            .with(fragment)
+            .load(portrait)
+            .placeholder(R.mipmap.avatar_def)
+            .transforms(new CenterCrop(), new RoundedCorners(UIUtils.dip2Px(fragment.getContext(), 4)))
+            .into(portraitImageView);
+        nameTextView.setText(name);
+    }
 }
