25e8010c17fe05c7ec05415116f13fef8b1e6466
imndx <imndxx@gmail.com>
2025-04-22 11:27:40 +0800

    上架需求

--- chat/src/main/java/cn/wildfire/chat/app/setting/SettingActivity.java
+++ chat/src/main/java/cn/wildfire/chat/app/setting/SettingActivity.java
@@ -73,7 +73,10 @@ public class SettingActivity extends WfcBaseActivity {
         //不要清除session，这样再次登录时能够保留历史记录。如果需要清除掉本地历史记录和服务器信息这里使用true
         ChatManagerHolder.gChatManager.disconnect(true, false);
         SharedPreferences sp = getSharedPreferences(Config.SP_CONFIG_FILE_NAME, Context.MODE_PRIVATE);
-        sp.edit().clear().apply();
+        sp.edit()
+            .clear()
+            .putBoolean("hasReadUserAgreement", true)
+            .apply();
 
         sp = getSharedPreferences("moment", Context.MODE_PRIVATE);
         sp.edit().clear().apply();
