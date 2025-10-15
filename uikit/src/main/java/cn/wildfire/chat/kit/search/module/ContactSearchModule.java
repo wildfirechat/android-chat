315469ace1c1ed90cf1ed0f8903d1e2d5b35c4e7
imndx <imndxx@gmail.com>
2025-10-15 17:41:38 +0800

    支持拼音搜索联系人

--- uikit/src/main/java/cn/wildfire/chat/kit/search/module/ContactSearchModule.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/search/module/ContactSearchModule.java
@@ -12,17 +12,23 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import cn.wildfire.chat.kit.R;
 import cn.wildfire.chat.kit.WfcUIKit;
+import cn.wildfire.chat.kit.contact.ContactViewModel;
 import cn.wildfire.chat.kit.contact.model.UIUserInfo;
 import cn.wildfire.chat.kit.contact.viewholder.UserViewHolder;
 import cn.wildfire.chat.kit.conversation.ConversationActivity;
 import cn.wildfire.chat.kit.search.SearchableModule;
+import cn.wildfire.chat.kit.utils.PinyinUtils;
 import cn.wildfirechat.model.Conversation;
 import cn.wildfirechat.model.UserInfo;
-import cn.wildfirechat.remote.ChatManager;
 
 public class ContactSearchModule extends SearchableModule<UserInfo, UserViewHolder> {
     @Override
@@ -63,6 +69,38 @@ public class ContactSearchModule extends SearchableModule<UserInfo, UserViewHold
 
     @Override
     public List<UserInfo> search(String keyword) {
-        return ChatManager.Instance().searchFriends(keyword);
+        ContactViewModel contactViewModel = WfcUIKit.getAppScopeViewModel(ContactViewModel.class);
+        List<UIUserInfo> contacts = contactViewModel.getContacts();
+        List<UserInfo> results = new ArrayList<>();
+        keyword = keyword.toLowerCase();
+        boolean isEnglishOnly = isEnglishOnly(keyword);
+        if (isEnglishOnly) {
+            HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+            format.setCaseType(HanyuPinyinCaseType.UPPERCASE);
+            format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        }
+        for (UIUserInfo uiUserInfo : contacts) {
+            UserInfo info = uiUserInfo.getUserInfo();
+            if (info.displayName.toLowerCase().contains(keyword) || (info.friendAlias != null && info.friendAlias.toLowerCase().contains(keyword))) {
+                results.add(info);
+            } else if (isEnglishOnly) {
+                String displayNamePinyin = PinyinUtils.getPinyin(info.displayName);
+                String friendAliasPinyin = info.friendAlias == null ? "" : PinyinUtils.getPinyin(info.friendAlias);
+                String displayNameFirstLetter = PinyinUtils.getPinyinFirstLetter(info.displayName);
+                String friendAliasFirstLetter = info.friendAlias == null ? "" : PinyinUtils.getPinyinFirstLetter(info.friendAlias);
+                if (displayNamePinyin.toLowerCase().contains(keyword)
+                    || friendAliasPinyin.toLowerCase().contains(keyword)
+                    || displayNameFirstLetter.toLowerCase().contains(keyword)
+                    || friendAliasFirstLetter.toLowerCase().contains(keyword)) {
+                    results.add(info);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    private static boolean isEnglishOnly(String input) {
+        return input.matches("^[a-zA-Z0-9]+$");
     }
 }
