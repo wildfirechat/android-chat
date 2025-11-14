8b26393d3f2ff6fcc367aa205514799c7373d862
imndx <imndxx@gmail.com>
2025-11-14 18:36:08 +0800

    修改turn服务密码

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -44,7 +44,7 @@ public class Config {
         // 如果是高级版，请删除掉下面的配置项目，保持ICE_SERVERS为空数组就行。
         // 数组元素定义
         /*{"turn server uri", "userName", "password"}*/
-        {"turn:turn.wildfirechat.net:3478", "wfchat", "wfchat123"}
+        {"turn:turn.wildfirechat.net:3478", "wfchat", "wfchatpwd"}
     };
 
     // 文件传输助手用户ID，服务器有个默认文件助手的机器人，如果修改它的ID，需要客户端和服务器数据库同步修改
