bae075e222828700a1eead0c9f699de1da15a598
imndx <imndxx@gmail.com>
2026-04-18 22:56:22 +0800

    默认关闭水印

--- uikit/src/main/java/cn/wildfire/chat/kit/Config.java
+++ uikit/src/main/java/cn/wildfire/chat/kit/Config.java
@@ -155,7 +155,7 @@ public class Config {
     public static int AUDIO_MESSAGE_AMPLIFICATION_FACTOR = 3;
 
     // 是否开启水印功能
-    public static boolean ENABLE_WATER_MARK = true;
+    public static boolean ENABLE_WATER_MARK = false;
 
     // AI机器人ID
     public static String AI_ROBOT = "FireRobot";
